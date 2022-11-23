0c74f892c658b459e5769a9b67806c6682029a6e
ngrube0120 <nickster0120@gmail.com>
2022-11-23 20:48:39 +0000

    CircleCI Step 2 part 12

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -57,6 +57,6 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
-end
 
-config.eager_load = true
+  config.eager_load = true
+end
